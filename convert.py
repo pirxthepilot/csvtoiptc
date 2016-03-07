39f64eb60645b854f65b12564c66b93f6ba68c26
Joon Guillen <joon@modulogeek.com>
2016-03-06 21:56:27 -0600

    Ready to run! Though not tested yet..

--- convert.py
+++ convert.py
@@ -4,32 +4,59 @@
 #
 # Syntax: convert.py <csv file path>
 
-import csv, sys
+import csv, sys, re
 from subprocess import call
 
 # Init
 #
 
 basedir = '/home/joon/Documents'
+exiftool = '/usr/bin/exiftool'
 
 if len(sys.argv) != 2:
     print "Syntax: convert.py <csv file path>"
     sys.exit()
 
 inputcsv = str(sys.argv[1])
-skip_cols = ['Year', 'Folder location', 'Original filename', 'New filename']
+skip_cols = ['Year', 'Folder location', 'Original filename', 'New filename', 'CopyrightStatus']
+pathfix = re.compile(r"\./")
+spacefix= re.compile(r"\s")
 
 
 # Main prog
 #
 with open(inputcsv, 'rb') as csvfile:
     csvdict = csv.DictReader(csvfile)
+
     for row in csvdict:
-        exiftool_args = "" 
-        for col,value in row.iteritems():
-            if col in skip_cols:
+        
+        # File path fixes
+        filename = basedir + pathfix.sub('/', row['Original filename'])
+        #filename = spacefix.sub('\ ', filename)
+        
+        # Call exiftool
+        exiftool_cmd = [ 'exiftool' ] 
+
+        # Prep some tags
+        if row['CopyrightStatus'].lower() == 'copyrighted':
+            #row['xmpRights:Marked'] = 'True'
+            row['Marked'] = 'True'
+        else:
+            #row['xmpRights:Marked'] = 'False'
+            row['Marked'] = 'True'
+
+        # Set tag args
+        for tag,value in row.iteritems():
+            if tag in skip_cols:
                 continue
-            exiftool_args += col + ': "' + value + '" '
-        print '========================='
-        #print exiftool_args
-        call(["echo", exiftool_args])
+            if value == "":
+                continue
+            exiftool_cmd.append('-' + tag + '="' + value + '"')
+        
+        # Filename
+        exiftool_cmd.append('"' + filename + '"')
+
+        # Run!
+        print 'Processing ' + filename + '...'
+        call(exiftool_cmd)
+        #print exiftool_cmd
