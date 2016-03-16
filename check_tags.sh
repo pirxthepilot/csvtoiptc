7d9c45a7114e31b8e15c5bdccbdda2638b41032b
joon <pirxthepilot@users.noreply.github.com>
2016-03-15 21:59:42 -0500

    Quick and dirty check tag script

---
+++ check_tags.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Check tags
+
+BASEDIR='/var/run/media/joon/GSV Sleeper Service/Byahero'
+
+while read -r file; do
+    if [[ $file =~ \.(mov|mp4|m4v|wmv|mpg|m2p|flv|avi)$ ]]; then
+        printf "$(basename "$file"),"
+        echo "$( exiftool -SubjectCode "$file" | awk -F ": " '{ print $2 }' )"
+    fi
+done <<< "$( find "$BASEDIR" -type f )"
+
+#shit=$( find "$BASEDIR" -type f )
+#echo "$shit"
