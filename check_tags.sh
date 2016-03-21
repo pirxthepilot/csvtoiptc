8c0e1a14ddc4b5a1d25981024a6ea9026797be2a
Joon Guillen <joon@modulogeek.com>
2016-03-21 00:19:38 -0500

    more stuff

--- check_tags.sh
+++ check_tags.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 # Check tags
 
-BASEDIR='/var/run/media/joon/GSV Sleeper Service/Byahero'
+BASEDIR='/var/run/media/joon/BYAHERO/Byahero'
 
 while read -r file; do
     if [[ $file =~ \.(mov|mp4|m4v|wmv|mpg|m2p|flv|avi)$ ]]; then
         printf "$(basename "$file"),"
-        echo "\"$( exiftool -SubjectCode "$file" | awk -F ": " '{ print $2 }' )\""
+        echo "\"$( exiftool -Description "$file" 2>/dev/null | awk -F ": " '{ print $2 }' )\""
     fi
 done <<< "$( find "$BASEDIR" -type f )"
 
--- file_list.sh
+++ file_list.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 # Check tags
 
-BASEDIR='/var/run/media/joon/GSV Sleeper Service/Byahero'
+#BASEDIR='/var/run/media/joon/BYAHERO'
+BASEDIR='/var/run/media/joon/BYAHERO/Byahero'
 
 while read -r file; do
-    printf "$(basename "$file"),"
-    echo "$file"
+    file_base="$(basename "$file")"
+    #if ! [[ "$file" =~ joon\/BYAHERO\/Byahero\/ ]] && ! [[ "$file" =~ \$RECYCLE ]] && ! [[ "$file" =~ \.Spotlight- ]] && ! [[ "$file_base" =~ ^\. ]] && ! [[ "$file_base" =~ ^Thumbs\.db ]]; then
+    if ! [[ "$file" =~ \$RECYCLE ]] && ! [[ "$file" =~ \.Spotlight- ]] && ! [[ "$file_base" =~ ^\. ]] && ! [[ "$file_base" =~ ^Thumbs\.db ]] ; then
+        printf "$file_base,"
+        echo "$file"
+    fi
 done <<< "$( find "$BASEDIR" -type f )"
 
